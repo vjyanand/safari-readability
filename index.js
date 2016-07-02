f15167296ea5af2c66d4d1bf998bba9197691d48
Vjy <vijay@airboxapp.co>
2016-07-02 10:33:50 +0530

    Clean up

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var path = require('path');
 var webdriver = require('selenium-webdriver');
 var chromeCapabilities = webdriver.Capabilities.chrome();
 var chromeOptions = {
-  'args': ['--test-type', '--start-maximized', '--allow-running-insecure-content', '--disable-web-security']
+  'args': ['--test-type', '--start-maximized', '--allow-running-insecure-content', '--disable-web-security', '--user-data-dir=/tmp']
 };
 
 chromeCapabilities.set('chromeOptions', chromeOptions);
 
 module.exports = function(url, cb) {
-  var fResult = {};
+  var finalResult = {};
   fs.readFile(path.resolve(__dirname, 'safari.js'), 'utf8', function(error, safariJS) {
     if (error) {
       return cb(error);
@@ -22,7 +22,7 @@ module.exports = function(url, cb) {
       driver.get(url);
       driver.executeAsyncScript(function(js, _cb) {
         eval(js);
-        if (ReaderArticleFinderJS.isReaderModeAvailable()) {
+        if (ReaderArticleFinderJS && ReaderArticleFinderJS.isReaderModeAvailable()) {
           var article = ReaderArticleFinderJS.adoptableArticle();
           var title = ReaderArticleFinderJS.articleTitle();
           return _cb({
@@ -30,18 +30,18 @@ module.exports = function(url, cb) {
             'title': title
           });
         }
-        return _cb({});
+        return _cb({'err': true});
       }, safariJS).then(function(result) {
         result.url = url;
-        fResult = result;
+        finalResult = result;
       });
     } catch (err) {
-      console.log("DRIVER QUIT ON ERROR:");
+      console.log("Driver quit on error");
     }
 
     driver.quit().then(function() {
-      console.log("DRIVER QUIT SUCCESS:");
-      return cb(fResult);
+      console.log("Driver quit on success");
+      return cb(finalResult);
     });
 
   });
